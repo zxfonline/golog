a867f5e13818a92028a79a768ff0ec5dfa4c5b53
zxfonline <zxfonline@sina.com>
2016-08-25 14:54:38 +0800

    change head
    change head

--- dailyrotate.go
+++ dailyrotate.go
@@ -1,5 +1,5 @@
-// Copyright 2014 by caixw, All rights reserved.
-// Use of this source code is governed by a MIT
+// Copyright 2016 zxfonline@sina.com. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 package golog
 
